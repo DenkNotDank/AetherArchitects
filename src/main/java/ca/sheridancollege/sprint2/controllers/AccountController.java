f742c0c64c6ba8c4f812a32db7caca71bdd0e1b0
DenkNotDank <77640625+DenkNotDank@users.noreply.github.com>
2024-09-14 19:39:34 -0400

    -Added changeUserInfo() to AccountController -Mapped form to changeUserInfo post req

--- src/main/java/ca/sheridancollege/sprint2/controllers/AccountController.java
+++ src/main/java/ca/sheridancollege/sprint2/controllers/AccountController.java
@@ -3,14 +3,36 @@ package ca.sheridancollege.sprint2.controllers;
 import ca.sheridancollege.sprint2.database.DatabaseAccess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/account")
+@Controller
 public class AccountController {
 
     @Autowired
     @Lazy
     private DatabaseAccess da;
+
+    @PostMapping("/changeUserInfo")
+    public String changeUserInfo(
+            @RequestParam(name = "firstName") String firstName,
+            @RequestParam(name = "lastName") String lastName,
+            @RequestParam(name = "phone") long phone,
+            @RequestParam(name = "secondaryEmail", required = false, defaultValue = "") String secondaryEmail,
+            @RequestParam(name = "province") String province,
+            @RequestParam(name = "city") String city,
+            @RequestParam(name = "postalCode", required = false, defaultValue = "") String postalCode,
+                                 Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("username", auth.getName());
+        if (da.findUserAccount(auth.getName()) != null) {
+            model.addAttribute("userExists", true);
+        }
+        return "/myAccount";
+    }
+
 
 }
